60ffe951f24fb45ccec920677e1362058ed29001
MarlonSouzaOliveira <marlonsouza0@gmail.com>
2026-04-28 00:12:07 -0300

    ok

--- test/test.py
+++ test/test.py
@@ -1,10 +1,7 @@
-from unittest import result
-
 from src.main import *
 from unittest.mock import patch
-
 import pytest
-import pytest_asyncio
+
 
 @pytest.mark.asyncio
 async def test_root():
@@ -24,28 +21,29 @@ async def test_funcaoteste():
 async def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
     result = await create_estudante(estudante_teste)
-    assert estudante_teste == result
+
+    assert result == estudante_teste
 
 
 @pytest.mark.asyncio
 async def test_update_estudante_negativo():
     result = await update_estudante(-5)
-    assert not result
+    assert result is False
 
 
 @pytest.mark.asyncio
 async def test_update_estudante_positivo():
     result = await update_estudante(10)
-    assert result
+    assert result is True
 
 
 @pytest.mark.asyncio
 async def test_delete_estudante_negativo():
     result = await delete_estudante(-5)
-    assert not result
+    assert result is False
 
 
 @pytest.mark.asyncio
 async def test_delete_estudante_positivo():
     result = await delete_estudante(5)
-    assert result
+    assert result is True
